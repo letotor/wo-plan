a8e506ad98fe53e25569fd0c0c521b9751004139
letotor <v.degermain@gmail.com>
2026-03-05 17:40:29 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 export default defineNuxtConfig({
-  ssr: true,
+ssr: true,
+ vite: {
+  ssr: {
+    noExternal: ['ag-grid-community', 'ag-grid-vue3']
+  }
+},
   css: ['~/assets/css/main.css'],
   nitro: {
     preset: 'netlify'
